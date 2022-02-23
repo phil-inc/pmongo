c14868616aef07d41f9131e2dc343401da90fda6
Aditya Kumar Varanasi <aditya@macbook-pro.myfiosgateway.com>
2022-02-22 23:47:07 -0500

    tests

--- test/mongo_connnect_test.go
+++ test/mongo_connnect_test.go
@@ -27,6 +27,26 @@ func TestConnectivity(t *testing.T) {
 	osl := new(OrderStatusMappingLookUpInfo)
 	core.Connection().Find(ctx, q, osl)
 	assertcommon(osl.MfrStatus, "mfr", t, "TestConnectivity")
+	dispose(ctx, core.Connection())
+}
+
+func TestConnectivitySecondaryConnectivityTest(t *testing.T) {
+	ctx := context.Background()
+	os.Setenv("db.main.url", "mongodb://mongo:mongo@mongodb:27017")
+	os.Setenv("db.main.name", "root-db")
+	config := core.DBConfig{HostURL: "mongodb://mongo:mongo@mongodb:27017", DBName: "root-db"}
+	core.SetupMongoDB(config)
+	assertcommon(core.Connection() != nil, true, t, "TestConnectivity")
+
+	data_setup(ctx, core.Connection(), "is", "mfr")
+
+	q := core.Q{
+		"internalStatus": "is",
+	}
+	osl := new(OrderStatusMappingLookUpInfo)
+	core.SecondaryConnection().Find(ctx, q, osl)
+	assertcommon(osl.MfrStatus, "mfr", t, "TestConnectivity")
+	dispose(ctx, core.Connection())
 
 }
 
@@ -35,8 +55,8 @@ func TestMultiConnectivity(t *testing.T) {
 	config1 := core.DBConfig{HostURL: "mongodb://mongo:mongo@mongodb:27017", DBName: "root-db"}
 	config2 := core.DBConfig{HostURL: "mongodb://mongo:mongo@mongodb:27017", DBName: "card-db"}
 	core.SetupMongoDB(config1, config2)
-	assertcommon(core.ConnectionByName("root-db") != nil, true, t, "TestConnectivity")
-	assertcommon(core.ConnectionByName("card-db") != nil, true, t, "TestConnectivity")
+	assertcommon(core.ConnectionByName("root-db") != nil, true, t, "TestMultiConnectivity")
+	assertcommon(core.ConnectionByName("card-db") != nil, true, t, "TestMultiConnectivity")
 
 	data_setup(ctx, core.ConnectionByName("root-db"), "is", "mfr")
 
@@ -45,7 +65,7 @@ func TestMultiConnectivity(t *testing.T) {
 	}
 	osl := new(OrderStatusMappingLookUpInfo)
 	core.ConnectionByName("root-db").Find(ctx, q, osl)
-	assertcommon(osl.MfrStatus, "mfr", t, "TestConnectivity")
+	assertcommon(osl.MfrStatus, "mfr", t, "TestMultiConnectivity")
 
 	data_setup(ctx, core.ConnectionByName("card-db"), "is2", "mfr2")
 
@@ -54,15 +74,51 @@ func TestMultiConnectivity(t *testing.T) {
 	}
 	osl2 := new(OrderStatusMappingLookUpInfo)
 	core.ConnectionByName("card-db").Find(ctx, q, osl2)
-	assertcommon(osl2.MfrStatus, "mfr2", t, "TestConnectivity")
+	assertcommon(osl2.MfrStatus, "mfr2", t, "TestMultiConnectivity")
 
 	osl = new(OrderStatusMappingLookUpInfo)
 	core.ConnectionByName("root-db").Find(ctx, core.Q{"internalStatus": "is2"}, osl)
-	assertcommon(osl.MfrStatus, "", t, "TestConnectivity")
+	assertcommon(osl.MfrStatus, "", t, "TestMultiConnectivity")
 
 	osl = new(OrderStatusMappingLookUpInfo)
 	core.ConnectionByName("card-db").Find(ctx, core.Q{"internalStatus": "is"}, osl)
-	assertcommon(osl.MfrStatus, "", t, "TestConnectivity")
+	assertcommon(osl.MfrStatus, "", t, "TestMultiConnectivity")
+	dispose(ctx, core.ConnectionByName("root-db"))
+	dispose(ctx, core.ConnectionByName("card-db"))
+}
+
+func TestMultiSecondaryDbConnectivity(t *testing.T) {
+	ctx := context.Background()
+	config1 := core.DBConfig{HostURL: "mongodb://mongo:mongo@mongodb:27017", DBName: "root-db"}
+	config2 := core.DBConfig{HostURL: "mongodb://mongo:mongo@mongodb:27017", DBName: "card-db"}
+	core.SetupMongoDB(config1, config2)
+	data_setup(ctx, core.ConnectionByName("root-db"), "is", "mfr")
+
+	q := core.Q{
+		"internalStatus": "is",
+	}
+	osl := new(OrderStatusMappingLookUpInfo)
+	core.SecondaryConnectionByName("root-db").Find(ctx, q, osl)
+	assertcommon(osl.MfrStatus, "mfr", t, "TestMultiSecondaryDbConnectivity")
+
+	data_setup(ctx, core.ConnectionByName("card-db"), "is2", "mfr2")
+
+	q = core.Q{
+		"internalStatus": "is2",
+	}
+	osl2 := new(OrderStatusMappingLookUpInfo)
+	core.SecondaryConnectionByName("card-db").Find(ctx, q, osl2)
+	assertcommon(osl2.MfrStatus, "mfr2", t, "TestMultiSecondaryDbConnectivity")
+
+	osl = new(OrderStatusMappingLookUpInfo)
+	core.SecondaryConnectionByName("root-db").Find(ctx, core.Q{"internalStatus": "is2"}, osl)
+	assertcommon(osl.MfrStatus, "", t, "TestMultiSecondaryDbConnectivity")
+
+	osl = new(OrderStatusMappingLookUpInfo)
+	core.SecondaryConnectionByName("card-db").Find(ctx, core.Q{"internalStatus": "is"}, osl)
+	assertcommon(osl.MfrStatus, "", t, "TestMultiSecondaryDbConnectivity")
+	dispose(ctx, core.SecondaryConnectionByName("root-db"))
+	dispose(ctx, core.SecondaryConnectionByName("card-db"))
 }
 
 func assertcommon(actual interface{}, expected interface{}, t *testing.T, testCase string) {
@@ -70,8 +126,8 @@ func assertcommon(actual interface{}, expected interface{}, t *testing.T, testCa
 	fmt.Println(result)
 	if !result {
 		t.Fatal(errors.New(fmt.Sprintf("%s failed", testCase)))
-		t.Fatal(errors.New(fmt.Sprintf("%s failed", testCase)))
-
+	} else {
+		fmt.Printf(testCase)
 	}
 }
 
@@ -80,6 +136,11 @@ func data_setup(ctx context.Context, conn *core.DBConnection, is, mfr string) {
 		MfrStatus: mfr,
 	}
 	conn.Save(ctx, o)
+}
+
+func dispose(ctx context.Context, conn *core.DBConnection) {
+	osl := new(OrderStatusMappingLookUpInfo)
+	conn.RemoveAll(ctx, core.Q{}, osl)
 }
 
 type OrderStatusMappingLookUpInfo struct {
